1a12360f2ec86cab726094993a7599b82e0318ca
matth <lion.delelis@gmail.com>
2019-07-19 00:38:25 +0200

    mouvement

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <fstream>
 #include <SDL/SDL.h>
+#include <cmath>
 
-#define FPS 30
-#define SCREEN_WIDTH 500
-#define SCREEN_HEIGHT 700
+#include <chrono>
+#include <thread>
+//this_thread::sleep_for();
+typedef std::chrono::high_resolution_clock::time_point time_point;
 
-void setPixel(SDL_Surface *screen, int x, int y, Uint32 color);
-void drawLine(SDL_Surface *screen, int x, int y, int w, int h, Uint32 color);
+#include "snake.h"
+
+#define FPS 30.0
+
+using namespace std;
 
 int main ( int argc, char** argv )
 {
@@ -15,29 +20,75 @@ int main ( int argc, char** argv )
 	// initialize SDL video
 	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-        	cout << "init sdl :" << SDL_GetError() << endl;
+        	log << "init sdl :" << SDL_GetError() << endl;
         	return 1;
 	}
 	// make sure SDL cleans up before exit
 	atexit(SDL_Quit);
 	
-    	// create a new window
-    	SDL_Surface* screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
-    	if (!screen)
-    	{
-        	log << "Unable to set video: " << SDL_GetError() << endl;
+    // create a new window
+    SDL_Surface* screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
+	if (!screen)
+	{
+    	log << "Unable to set video: " << SDL_GetError() << endl;
 		return 1;
 	}
+
+	SDL_WM_SetCaption("SNAKE",NULL);
+
+	//main loop
+	bool continuer = 1;
+	SDL_Event event;
+
+	//snake
+	Snake snake(20,20);
+
+	double fpsDirect = 0.0;
+	time_point timeBefore, timeNow, timeFirst = chrono::high_resolution_clock::now();
+
+	while(continuer)
+	{
+		timeBefore = chrono::high_resolution_clock::now();
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_QUIT:
+					continuer = 0;
+					break;
+				case SDL_KEYDOWN:
+					switch(event.key.keysym.sym)
+					{
+						case SDLK_DOWN:
+							snake.move(3);
+							break;
+						case SDLK_UP:
+							snake.move(2);
+							break;
+						case SDLK_RIGHT:
+							snake.move(1);
+							break;
+						case SDLK_LEFT:
+							snake.move(0);
+							break;
+					}
+					break;
+			}
+		}
+		SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,0,0,0));
+		
+		snake.draw(screen);
+
+		SDL_Flip(screen);
+		//actualisation
+
+		timeNow = chrono::high_resolution_clock::now();
+		if(chrono::duration_cast<chrono::milliseconds>(timeNow-timeBefore).count()>1000.0/FPS)
+		{
+			this_thread::sleep_for(chrono::milliseconds((unsigned int)(chrono::duration_cast<chrono::milliseconds>(timeNow-timeBefore).count()-1000.0/FPS)));
+		}
+
+		fpsDirect = 1000.0/chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now()-timeBefore).count();
+	}
 	return 0;
-}
-
-
-void setPixel(SDL_Surface *screen, int x, int y, Uint32 color)
-{
-	*((Uint32*)(screen->pixels)+x+y*screen->w) = color;
-}
-
-void drawLine(SDL_Surface *screen, int x, int y, int w, int h, Uint32 color)
-{
-
 }
